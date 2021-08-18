b4906f5a4e54a44f0b3ecf3017d730144570abb3
Otto Modinos <ottomodinos@gmail.com>
2021-08-18 14:16:46 +0300

    cosmetic changes

--- grammar.js
+++ grammar.js
@@ -5,7 +5,7 @@ module.exports = grammar({
 
   externals: $ => [
     $.field,
-    $.colon
+    $.colon,
   ],
 
   rules: {
@@ -21,7 +21,7 @@ module.exports = grammar({
       $._conditional,
       $.hash_function_definition,
       $.do_statement,
-      $.comment
+      $.comment,
     ),
 
     _function: $ => choice(
@@ -35,13 +35,13 @@ module.exports = grammar({
       $.var_definition,
       $.global_definition,
       $.set,
-      $.tset
+      $.tset,
     ),
 
     _iterator: $ => choice(
       $.each,
       $.for,
-      $.while
+      $.while,
     ),
 
     _conditional: $ => choice(
@@ -54,28 +54,28 @@ module.exports = grammar({
       '(',
       'require',
       repeat(choice($.field, $.string)),
-      ')'
+      ')',
     ),
 
     do_statement: $ => seq(
       '(',
       'do',
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     when_statement: $ => seq(
       '(',
       'when',
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     if_statement: $ => seq(
       '(',
       'if',
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     match_statement: $ => seq(
@@ -90,7 +90,7 @@ module.exports = grammar({
       'each',
       $.each_clause,
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     each_clause: $ => seq(
@@ -98,7 +98,7 @@ module.exports = grammar({
       $.identifier,
       $.identifier,
       $.function_call,
-      ']'
+      ']',
     ),
 
     for: $ => seq(
@@ -106,7 +106,7 @@ module.exports = grammar({
       'for',
       $.for_clause,
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     for_clause: $ => seq(
@@ -115,7 +115,7 @@ module.exports = grammar({
       $._statement,
       $._statement,
       optional($._statement),
-      ']'
+      ']',
     ),
 
     while: $ => seq(
@@ -123,7 +123,7 @@ module.exports = grammar({
       'while',
       field('condition', $._statement),
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     let_definition: $ => seq(
@@ -131,35 +131,35 @@ module.exports = grammar({
       'let',
       $.assignments,
       repeat($._statement),
-      ')'
+      ')',
     ),
 
     local_definition: $ => seq(
       '(',
       'local',
       choice($.assignment, $.multi_value_assignment),
-      ')'
+      ')',
     ),
 
     var_definition: $ => seq(
       '(',
       'var',
       choice($.assignment, $.multi_value_assignment),
-      ')'
+      ')',
     ),
 
     global_definition: $ => seq(
       '(',
       'global',
       choice($.assignment, $.multi_value_assignment),
-      ')'
+      ')',
     ),
 
     set: $ => seq(
       '(',
       'set',
       choice($.assignment, $.multi_value_assignment),
-      ')'
+      ')',
     ),
 
     tset: $ => seq(
@@ -168,30 +168,53 @@ module.exports = grammar({
       optional(choice($.table, $.identifier)),
       choice($.identifier, $.field, $.string),
       choice($._statement),
-      ')'
+      ')',
     ),
 
-    assignments: $ => seq('[', repeat(choice($.multi_value_assignment, $.assignment)), ']'),
+    assignments: $ => seq(
+      '[',
+      repeat(choice(
+        $.assignment,
+        $.multi_value_assignment,
+      )),
+      ']',
+    ),
 
-    assignment: $ => seq(choice($.identifier, $.field_expression), $._statement),
+    assignment: $ => seq(
+      choice(
+        $.identifier,
+        $.field_expression,
+      ),
+      $._statement,
+    ),
 
-    multi_value_assignment: $ => seq($.value_list, $._statement),
+    multi_value_assignment: $ => seq(
+      $.value_list,
+      $._statement,
+    ),
 
-    value_list: $ => seq('(', repeat(choice($.identifier, $.field_expression)), ')'),
+    value_list: $ => seq(
+      '(',
+      repeat(choice(
+        $.identifier,
+        $.field_expression,
+      )),
+      ')',
+    ),
 
     hash_function_definition: $ => choice(
       seq(
-        '(', 
+        '(',
         'hashfn',
         repeat($._statement),
-        ')'
+        ')',
       ),
       seq(
         '#',
         choice(
           $.function_call,
           $.identifier,
-          $.sequential_table
+          $.sequential_table,
         ),
       ),
     ),
@@ -200,20 +223,23 @@ module.exports = grammar({
       '(',
       'fn',
       $._function_body,
-      ')'
+      ')',
     ),
 
     lambda_definition: $ => seq(
       '(',
       choice('lambda', 'λ'),
       $._function_body,
-      ')'
+      ')',
     ),
 
     _function_body: $ => seq(
-      optional(field('name', choice($.identifier, $.field_expression))),
+      optional(field('name', choice(
+        $.identifier,
+        $.field_expression,
+      ))),
       $.parameters,
-      field('body', repeat($._statement))
+      field('body', repeat($._statement)),
     ),
 
     parameters: $ => seq('[', repeat($._expression), ']'),
@@ -222,19 +248,19 @@ module.exports = grammar({
       '(',
       field('name', choice(
         $.string,
-        $.field_expression, 
-        $.identifier, 
+        $.field_expression,
+        $.identifier,
         alias($._operator, $.identifier),
-        alias($._keyword, $.identifier)
+        alias($._keyword, $.identifier),
       )),
       optional(repeat($._statement)),
-      ')'
+      ')',
     ),
 
     sequential_table: $ => seq(
       '[',
       repeat($._statement),
-      ']'
+      ']',
     ),
 
     table: $ => seq(
@@ -242,15 +268,15 @@ module.exports = grammar({
       repeat(
         seq(
           choice(
-            alias($.colon, $.identifier), 
-            $.string, 
-            $.field, 
-            $.identifier
+            alias($.colon, $.identifier),
+            $.string,
+            $.field,
+            $.identifier,
           ),
-          $._statement
-        )
+          $._statement,
+        ),
       ),
-      '}'
+      '}',
     ),
 
     _expression: $ => choice(
@@ -273,54 +299,42 @@ module.exports = grammar({
     string: $ => seq(
       '"',
       repeat(/(\\")|(.)/),
-      '"'
+      '"',
     ),
 
     quoted_value: $ => seq(
-      choice("'", "`"),
-      $._statement
+      choice('\'', '`'),
+      $._statement,
     ),
 
     unquoted_value: $ => seq(
       choice(','),
-      $.identifier
+      $.identifier,
     ),
 
     field_expression: $ => prec(2, seq(
       choice(
-        $.identifier, 
-        alias($._keyword, $.identifier)
+        $.identifier,
+        alias($._keyword, $.identifier),
       ),
       choice(
-        repeat1(seq(".", $.identifier)),
+        repeat1(seq('.', $.identifier)),
       ),
     )),
 
     _operator: $ => choice(
       $._arithmetic_operator,
-      $._comparison_operator,
       $._boolean_operator,
+      $._comparison_operator,
       $._threading_macro,
       $._misc_operator,
-      alias($.colon, $.identifier)
+      alias($.colon, $.identifier),
     ),
 
-    _arithmetic_operator: $ => choice(
-      '+', '-', '*', '%', '/', '//', '^'
-    ),
-
-    _comparison_operator: $ => choice(
-      '>', '<', '>=', '<=', '=', 'not='
-    ),
-
-    _threading_macro: $ => choice(
-      '->', '->>', '-?>', '-?>>',
-    ),
-
-    _boolean_operator: $ => choice(
-      'and', 'or', 'not'
-    ),
-
+    _arithmetic_operator: $ => choice('+', '-', '*', '%', '/', '//', '^'),
+    _boolean_operator: $ => choice('and', 'or', 'not'),
+    _comparison_operator: $ => choice('>', '<', '>=', '<=', '=', 'not='),
+    _threading_macro: $ => choice('->', '->>', '-?>', '-?>>'),
     _misc_operator: $ => choice('..', '.', '...'),
 
     boolean: $ => choice('true', 'false'),
@@ -379,13 +393,13 @@ module.exports = grammar({
       'varg?',
       'xpcall',
       'string',
-      'table'
+      'table',
     ),
 
     identifier: $ => /([_\?A-Za-z<>][_\?\-A-Za-z0-9<>#\!]*)|(\$([1-9])?)/,
 
     number: $ => /([-])?\d+(\.\d+)?/,
 
-    comment: $ => /;.*/
-  }
+    comment: $ => /;.*/,
+  },
 });
